d54799b710cb2c7c68de8ff24702d3457ffd1908
m-ghiringhelli <mghiring@gmail.com>
2022-02-08 17:48:45 -0800

    only managed to copy down one function

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,17 +1,17 @@
-const SUPABASE_URL = "https://njovlynmvtgkoexzepbb.supabase.co";
+const SUPABASE_URL = 'https://qurnakdkijnyhcuzbmgb.supabase.co';
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTMxNjU3MSwiZXhwIjoxOTU2ODkyNTcxfQ.4sFYy_xhbdh7hVgaEmH_ILGTIl6rsqtUgzAJsizIxkA";
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1cm5ha2RraWpueWhjdXpibWdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQzNDE0NDMsImV4cCI6MTk1OTkxNzQ0M30.IltjE-P3qd05gdR8QTQqmRASPhJmLREfwTW5uFWm7c8';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 function checkError({ data, error }) {
-  return error ? console.error(error) : data;
+    return error ? console.error(error) : data;
 }
 
 export async function getMovies() {
   // return the list of all movies
-  const resp = await client.from("movies").select("*");
-  return checkError(resp);
+    const resp = await client.from('movies').select('*');
+    return checkError(resp);
 }
 
 export async function getMoviesWithDirector() {
@@ -28,6 +28,13 @@ export async function getMovieById(id) {
 
 export async function getMovieByTitle(title) {
   // return the movie with the given title
+    const resp = await client
+    .from('movies)'
+    .select('*');
+    .like('title', title)
+    .single();
+  console.log('getMovieByTitle', resp);
+  return checkError(resp;)
 }
 
 export async function getOldestMovie() {
